492b1e3629026e69c23ce4de3c852c037fc7a368
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2018-03-15 13:53:18 +0300

    try add sonar and codecov

---
+++ check.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Exit on failure
+set -e
+
+if [ "$TRAVIS_BRANCH" = "ci" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    # run normal analysis
+    ./mvnw -P $SANITY_PROFILE \
+        package sonar:sonar \
+		-Dsonar.host.url=$SONAR_HOST_URL \
+		-Dsonar.login=$SONAR_TOKEN
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
+    # run PR analysis
+	./mvnw -P $SANITY_PROFILE \
+	    package sonar:sonar \
+		-Dsonar.host.url=$SONAR_HOST_URL \
+		-Dsonar.login=$SONAR_TOKEN \
+		-Dsonar.analysis.mode=preview \
+		-Dsonar.github.oauth=$GITHUB_TOKEN \
+		-Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+		-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
+else
+    # run tests
+    mvn -P $SANITY_PROFILE verify
+fi
